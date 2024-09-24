c52e2ac0baea0dac4eb036f6623fe58437b84637
Srinivetha18 <nivethajr@gmail.com>
2024-09-24 11:18:39 +0530

    navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => {
   return (
 
     <div className='nav'>
-        <h1></h1>
+        {/* <h1></h1> */}
         <ul className='nav-links' style={{backgroundColor:"red"}}>
             <Link to ='/' className='Fruitsandveg'>  
             <li>Fruits and veg</li> 
